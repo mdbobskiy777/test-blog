0cc8fef0c4ac1e5c4549f59fefaf8d662226b3a5
mdb_art <mdbobskiy777@gmail.com>
2021-01-15 18:58:50 +0200

    still trying add jslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,7 @@ module.exports =  {
         "plugin:@typescript-eslint/recommended"
     ],
     parserOptions:  {
-        ecmaVersion:  2018,  // Allows for the parsing of modern ECMAScript features
+        ecmaVersion:  "es6",  // Allows for the parsing of modern ECMAScript features
         sourceType:  'module',  // Allows for the use of imports
         ecmaFeatures:  {
             jsx:  true,  // Allows for the parsing of JSX
