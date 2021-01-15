c3cfe3b47145c052edaa2d28e33f9b207550ff28
mdb_art <mdbobskiy777@gmail.com>
2021-01-15 15:34:25 +0200

    added types to api.ts

--- api/api.ts
+++ api/api.ts
@@ -1,5 +1,12 @@
 import  axios from "axios"
+import {CommentType, PostType} from "../redux/reducers/postsReducer";
 
+
+type PostsAPIMyType = Array<PostType>
+type PostsAPIGetPostType = PostType
+type PostsAPICreateCommentType = CommentType
+type PostsAPICreatePostType = PostType
+type PostsAIPDeletePostType = {}
 const instance = axios.create({
     baseURL: `https://simple-blog-api.crew.red/`,
     responseType: "json",
@@ -10,30 +17,30 @@ const instance = axios.create({
 
 export const postsAPI = {
     getPosts() {
-        return instance.get('posts')
+        return instance.get<PostsAPIMyType>('posts')
             .then(res => res.data)
     },
-    getPost(id) {
-        return instance.get(`posts/${id}?_embed=comments`).then(res => res.data)
+    getPost(id:number) {
+        return instance.get<PostsAPIGetPostType>(`posts/${id}?_embed=comments`).then(res => res.data)
     },
-    createComment(id, text) {
+    createComment(id:number, text:string) {
         const raw = {
-            postId: parseInt(id,10),
+            postId: id,
             body: text
         }
-        return instance.post(`comments`,raw )
+        return instance.post<PostsAPICreateCommentType>(`comments`,raw )
     },
-    createPost(title, body){
-        return instance.post(`posts`,{
+    createPost(title:string, body:string){
+        return instance.post<PostsAPICreatePostType>(`posts`,{
             title,
             body
         }).then(res=>res.data)
     },
     deletePost(id){
-        return instance.delete(`posts/${id}`)
+        return instance.delete<PostsAIPDeletePostType>(`posts/${id}`)
     },
-    updatePost(id,title, body){
-        return instance.put(`posts/${id}`, {
+    updatePost(id:number,title:string, body:string){
+        return instance.put<PostType>(`posts/${id}`, {
             title,
             body
         })
