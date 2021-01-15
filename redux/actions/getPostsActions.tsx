745c0ee4ecde85265db4b9cba9c93943acf4cb15
mdb_art <mdbobskiy777@gmail.com>
2021-01-15 15:19:30 +0200

    added types in store, actions, reducer

--- redux/actions/getPostsActions.tsx
+++ redux/actions/getPostsActions.tsx
@@ -1,61 +1,81 @@
 import {postsAPI} from "../../api/api";
+import {
+    ActionsType,
+    AddPostSuccess,
+    GetLatestPostsType,
+    GetPostType,
+    PostType
+} from "../reducers/postsReducer";
+import {ThunkAction} from "redux-thunk";
+import {AppStoreType} from "../reducers/rootReducer";
 
-export const  GET_LATEST_POSTS = "GET_LATEST_POSTS"
-export const  GET_POST = "GET_POST"
+export const GET_LATEST_POSTS = "GET_LATEST_POSTS"
+export const GET_POST = "GET_POST"
 export const ADD_POST_SUCCESS = "ADD_POST_SUCCESS"
 export const ADD_COMMENT_SUCCESS = "ADD_COMMENT_SUCCESS"
 
+
 //Action Creator
-export const get_posts = (posts) => ({
+export const get_posts = (posts: Array<PostType>): GetLatestPostsType => ({
     type: GET_LATEST_POSTS,
     posts: posts,
 })
-export const get_post = (post) => ({
+export const get_post = (post: PostType): GetPostType => ({
     type: GET_POST,
     post: post,
 })
-export const add_post_success = (isAdd) => ({
+export const add_post_success = (isAdd: boolean): AddPostSuccess => ({
     type: ADD_POST_SUCCESS,
-    addPostSuccess:isAdd
+    addPostSuccess: isAdd
 })
 
 /*export const add_comment_success = (isAdd) => ({
     type: ADD_COMMENT_SUCCESS,
     addCommentSuccess:isAdd
 })*/
-export const addPost = (title, body) => async dispatch => {
 
-    const responce = await postsAPI.createPost(title, body);
-    if(responce){
-         dispatch(add_post_success(true))
+export const addPost = (title: string, body: string): ThunkAction<Promise<void>, AppStoreType, undefined, ActionsType> =>
+    async (dispatch) => {
+
+        const responce = await postsAPI.createPost(title, body);
+        if (responce) {
+            dispatch(add_post_success(true))
+        }
+
     }
 
-}
+export const deletePost = (id: number): ThunkAction<Promise<void>, AppStoreType, undefined, ActionsType> =>
+    async dispatch => {
+        await postsAPI.deletePost(id);
+        return dispatch(getPosts())
 
-export const deletePost = (id) => async dispatch => {
-    await postsAPI.deletePost(id);
-    return dispatch(getPosts())
+    }
 
-}
+export const addComment = (id: number, text: string): ThunkAction<Promise<void>, AppStoreType, undefined, ActionsType> =>
+    async (dispatch) => {
 
-export const addComment = (id, text) => async dispatch => {
+        const responce = await postsAPI.createComment(id, text);
+        /*   if(responce){
+               dispatch(add_comment_success(true))
+           }*/
 
-    const responce = await postsAPI.createComment(id, text);
-    /*   if(responce){
-           dispatch(add_comment_success(true))
-       }*/
+    }
 
-}
+export const getPosts = (): ThunkAction<Promise<void>, AppStoreType, undefined, ActionsType> =>
+    async (dispatch) => {
 
-export const getPosts = () => async dispatch => {
-
-            const posts = await postsAPI.getPosts();
+        const posts = await postsAPI.getPosts();
+        /*
             return dispatch(get_posts(posts))
+        */
+        dispatch(get_posts(posts))
+    }
+export const getPost = (id): ThunkAction<Promise<void>, AppStoreType, undefined, ActionsType> =>
+    async (dispatch) => {
 
-}
-export const getPost = (id) => async dispatch => {
-
-    const post = await postsAPI.getPost(id);
-    return dispatch(get_post(post))
-
-}
+        const post = await postsAPI.getPost(id);
+        /*
+            return dispatch(get_post(post))
+        */
+        dispatch(get_post(post))
+    }
--- redux/reducers/postsReducer.tsx
+++ redux/reducers/postsReducer.tsx
@@ -1,6 +1,23 @@
 import {ADD_COMMENT_SUCCESS, ADD_POST_SUCCESS, GET_LATEST_POSTS, GET_POST} from "../actions/getPostsActions";
 
-const initialState = {
+export type CommentType = {
+    id:number,
+    postId:number,
+    body:string
+}
+export type PostType = {
+    id:number,
+    title:string,
+    body:string,
+    comments?: Array<CommentType>
+}
+export type InitialStateType = {
+    posts: Array<PostType> | null,
+    post: PostType | null,
+    addPostSuccess: boolean
+}
+
+const initialState: InitialStateType = {
     posts: null,
     post: null,
     addPostSuccess:false,
@@ -9,7 +26,22 @@ const initialState = {
 */
 }
 
-function postsReducer(state = initialState, action) {
+export type GetLatestPostsType = {
+    type: typeof GET_LATEST_POSTS,
+    posts: Array<PostType>
+}
+export type GetPostType = {
+    type: typeof GET_POST,
+    post: PostType
+}
+export type AddPostSuccess = {
+    type: typeof ADD_POST_SUCCESS,
+    addPostSuccess: boolean
+}
+
+export type ActionsType = GetLatestPostsType | GetPostType | AddPostSuccess
+
+function postsReducer(state = initialState, action: ActionsType) : InitialStateType {
     switch (action.type) {
         case GET_LATEST_POSTS:
             return {...state, posts: action.posts};
