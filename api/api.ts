c66adfb289cdac7613e0f0b7bc05673cb5fe4312
mdb_art <mdbobskiy777@gmail.com>
2021-01-18 03:00:14 +0200

    added return type i api responce, updated gitignore, updated some info in package.json

--- api/api.ts
+++ api/api.ts
@@ -18,30 +18,30 @@ const instance = axios.create({
 })
 
 export const postsAPI = {
-    getPosts() {
+    getPosts():Promise<PostsAPIMyType> {
         return instance.get<PostsAPIMyType>('posts')
             .then(res => res.data)
     },
-    getPost(id:number) {
+    getPost(id:number):Promise<PostType> {
         return instance.get<PostsAPIGetPostType>(`posts/${id}?_embed=comments`).then(res => res.data)
     },
-    createComment(id:number, text:string) {
+    createComment(id:number, text:string):Promise<any> {
         const raw = {
             postId: id,
             body: text
         }
         return instance.post<PostsAPICreateCommentType>(`comments`,raw )
     },
-    createPost(title:string, body:string){
+    createPost(title:string, body:string): Promise<PostType>{
         return instance.post<PostsAPICreatePostType>(`posts`,{
             title,
             body
         }).then(res=>res.data)
     },
-    deletePost(id:number){
+    deletePost(id:number):Promise<any>{
         return instance.delete<PostsAPIDeletePostType>(`posts/${id}`)
     },
-    updatePost(id:number,title:string, body:string){
+    updatePost(id:number,title:string, body:string):Promise<any>{
         return instance.put<PostType>(`posts/${id}`, {
             title,
             body
